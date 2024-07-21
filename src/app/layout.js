a1fa148bc1ace1b3ed217d528b7a94cb378e6dee
putrapratamma18 <putrapratamma18@gmail.com>
2024-07-21 22:09:05 +0700

    update appss

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import { Roboto } from 'next/font/google';
 
@@ -6,13 +5,6 @@ const roboto = Roboto({
   weight: ['400', '700'],
   subsets: ['latin']
 });
-=======
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
->>>>>>> 5e06176eb4014e5f90154783ec7aa0f7665779a4
 export const metadata = {
   title: "TryoutApp",
   description: "TryoutApp",
@@ -21,11 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-<<<<<<< HEAD
       <body className={roboto.className}>{children}</body>
-=======
-      <body className={inter.className}>{children}</body>
->>>>>>> 5e06176eb4014e5f90154783ec7aa0f7665779a4
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,4 @@
 import LoginPages from "@/pages/LoginPages/LoginPages";
-<<<<<<< HEAD
-=======
-import "./globals.css";
->>>>>>> 5e06176eb4014e5f90154783ec7aa0f7665779a4
 
 export default function Home() {
   return (
